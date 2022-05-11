f2cafad4635ca833811c82c21e29ebef1c504650
Vasyl <maslovvasil@gmail.com>
2022-05-11 10:28:31 +0300

    hw-7.2
    hw7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(createGalleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+const cardsMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+
+console.log(galleryItems);
+function createGalleryItems(galleryItems){
+  return galleryItems.map(({ preview, original, description }) => {
+    return  `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+             <img
+             class="gallery__image" src="${preview}"
+             alt="${description}"
+             />
+        </a>
+    </div> `;
+    })
+    .join(``);
+}
+
+const gallery = new SimpleLightbox('.gallery a', { 
+    captionsData: `alt`, captionPosition: 'bottom', captionDelay: `250ms`,
+});
+gallery.on(`show.simplelightbox`, function () {
+     
+});
